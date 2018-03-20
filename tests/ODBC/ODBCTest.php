4c59ae5c75dcaf9667eda1896fc154d28e09c4f5
proyectotau <ProyectoTAU2@gmail.com>
2018-03-20 17:59:39 +0100

    changed the foldername tests/ODBC

--- tests/ODBC/ODBCTest.php
+++ tests/ODBC/ODBCTest.php
@@ -8,9 +8,6 @@ include_once '/var/www/TAU/tauproject/web/config.inc.php'; // TODO: short tag fo
 
 class ODBCTest extends TestCase
 {
-    /*
-     * @ignore
-     */
     function test_unixODBC_is_available()
     {
        $this->assertNotFalse( $conn = odbc_connect(DB_ODBC, DB_USER, DB_PASS), 'unixODBC is not available');
@@ -18,12 +15,18 @@ class ODBCTest extends TestCase
 
     }
 
+    /**
+    * @depends test_unixODBC_is_available
+    */
     function test_ODBC_TAU_connection_is_available()
     {
         $this->assertNotFalse( $conn = odbc_connect(DB_ODBC, DB_USER, DB_PASS), 'ODBC TAU is not available');
         odbc_close($conn);
     }
 
+    /**
+    * @depends test_unixODBC_is_available
+    */
     function test_ODBC_INVENTARIO_connection_is_available()
     {
         $this->assertNotFalse( $conn = odbc_connect(Inventario2_DB_ODBC, DB_USER, DB_PASS), 'ODBC TAU is not available');
