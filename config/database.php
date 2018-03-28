eb90fffc383d99bb936fd403afd5ea2ca680cb76
ProyectoTAU <proyectotau2@gmail.com>
2018-03-28 10:12:51 +0200

    Remove offending comma again

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'tau_test'),
             'username' => env('MYSQL_USER', env('DB_USERNAME', 'root')),
-            'password' => env('MYSQL_PASSWORD', ,env('DB_PASSWORD', '')),
+            'password' => env('MYSQL_PASSWORD', env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_spanish_ci',
