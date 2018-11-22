d6f72cef9dc74f4ad3f4521079e8c99dcf7f65a7
proyectotau <proyectoTAU2@gmail.com>
2018-11-22 17:04:42 +0100

    Fixed Windows database scripts

--- database/migrations/bd_test/create_database.cmd
+++ database/migrations/bd_test/create_database.cmd
@@ -1,8 +1,11 @@
 set DB_CMD=mysql
-set DB_SERVER=localhost
-set DB_USER=root
+set DB_SERVER=%MYSQL_HOST%
+set DB_USER=%MYSQL_USER%
 set DB_PASS=%MYSQL_PASSWORD%
 REM set DB_DATABASE=
+if "%DB_SERVER%"=="" set DB_SERVER=localhost
+if "%DB_USER%"=="" set DB_USER=root
+if "%DB_PASS%"=="" set DB_PASS=test
 
-%DB_CMD% --line-numbers -v -v -v -h %DB_SERVER% -u %DB_USER% -p%DB_PASS% -e 'create database tau_test;'
-%DB_CMD% --line-numbers -v -v -v -h %DB_SERVER% -u %DB_USER% -p%DB_PASS% -e 'create database inventario_test;'
+%DB_CMD% --line-numbers -v -v -v -h %DB_SERVER% -u %DB_USER% -p%DB_PASS% -e "create database tau_test;"
+%DB_CMD% --line-numbers -v -v -v -h %DB_SERVER% -u %DB_USER% -p%DB_PASS% -e "create database inventario_test;"
--- database/migrations/bd_test/drop_database.cmd
+++ database/migrations/bd_test/drop_database.cmd
@@ -3,6 +3,9 @@ set DB_SERVER=%MYSQL_HOST%
 set DB_USER=%MYSQL_USER%
 set DB_PASS=%MYSQL_PASSWORD%
 REM set DB_DATABASE=
+if "%DB_SERVER%"=="" set DB_SERVER=localhost
+if "%DB_USER%"=="" set DB_USER=root
+if "%DB_PASS%"=="" set DB_PASS=test
 
-%DB_CMD% --line-numbers -v -v -v -h %DB_SERVER% -u %DB_USER% -p%DB_PASS% -e 'drop database IF EXISTS tau_test;'
-%DB_CMD% --line-numbers -v -v -v -h %DB_SERVER% -u %DB_USER% -p%DB_PASS% -e 'drop database IF EXISTS inventario_test;'
+%DB_CMD% --line-numbers -v -v -v -h %DB_SERVER% -u %DB_USER% -p%DB_PASS% -e "drop database IF EXISTS tau_test;"
+%DB_CMD% --line-numbers -v -v -v -h %DB_SERVER% -u %DB_USER% -p%DB_PASS% -e "drop database IF EXISTS inventario_test;"
