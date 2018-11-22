d12e6b6787baeef3290d6faafd441b5b315868ac
proyectotau <proyectoTAU2@gmail.com>
2018-11-22 11:52:13 +0100

    Fixed #3

--- Modules/Administration/Resources/views/partials/_navbar.blade.php
+++ Modules/Administration/Resources/views/partials/_navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- will be used to show navigation bar -->
 <nav class="navbar navbar-inverse">
     <div class="navbar-header">
-        <a class="navbar-brand" href="{{ URL::to('users') }}">Nerd Alert</a>
+        <a class="navbar-brand" href="{{ URL::to('/') }}">Nerd Alert</a>
     </div>
     <ul class="nav navbar-nav">
         <li><a href="{{ route('admin.users.index') }}">View All Users</a></li>
--- database/migrations/bd_test/create_tables.sh
+++ database/migrations/bd_test/create_tables.sh
@@ -6,9 +6,11 @@ DB_USER="${MYSQL_USER:-root}"
 DB_PASS="${MYSQL_PASSWORD:-test}"
 # set DB_DATABASE=
 
+echo -- TAUv1.sql
 $DB_CMD --line-numbers -v -v -v -h $DB_SERVER -u $DB_USER -p$DB_PASS < TAUv1.sql
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
 do
+ echo -- TAUv1.$i.sql
  $DB_CMD --line-numbers -v -v -v -h $DB_SERVER -u $DB_USER -p$DB_PASS < TAUv1.$i.sql
 done
